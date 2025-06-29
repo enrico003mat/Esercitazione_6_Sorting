51be88abcbd0b2181da3afca98ef7b3ae38e4d00
enrico003mat <s309484@studenti.polito.it>
2025-06-29 12:03:36 +0200

    Update SortingAlgorithm.hpp

--- src/SortingAlgorithm.hpp
+++ src/SortingAlgorithm.hpp
@@ -73,20 +73,22 @@ void InsertionSort(std::vector<T>& v)
     }
 
 }
-
+//funzione che costruisce heap
 template<Sortable T>
 void subf(std::vector<T>& v,int n,int i)
 {
 	int largest = i;
+	//figlio sx
     int l = 2 * i + 1; 
+	//figlio dx
     int r = 2 * i + 2; 
-
+	//imposto as larger figlio più grande del nodo corrente
     if (l < n && v[l] > v[largest])
         largest = l;
 
     if (r < n && v[r] > v[largest])
         largest = r;
-
+	// se un figlio è più grande swap e applico ricorsivamente
     if (largest != i) {
         std::swap(v[i], v[largest]);
         subf(v, n, largest);
@@ -99,13 +101,15 @@ void HeapSort(std::vector<T>& v)
 {
 
    int n = v.size();
-
+	//parto da fondo albero e sistemo sotto albero
+	//subf controlla se soddisfo prop heap
     for (int i = n / 2 - 1; i >= 0; i--)
         subf(v, n, i);
 
-    
+    //voglio mettere elt massimo in fondo a vettore
     for (int i = n - 1; i > 0; i--) {
         std::swap(v[0], v[i]);
+	    //ristabilisco propr heap
         subf(v, i, 0);        
     }
 }
